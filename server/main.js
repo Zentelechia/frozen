4b63eadb451235b34c83c1d9ef63799baab8eb79
zentelechia <zentelechia@gmail.com>
2019-08-28 23:37:09 +0300

    1

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import {
   Meteor
 } from 'meteor/meteor';
 
+var multipart = require('parse-multipart');
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
@@ -29,7 +31,8 @@ Router.route('/api/v1/data/push', function () {
 Router.route('/api/v1/file/upload', function () {
   var data = this.request;
   var res = this.response;
-  console.log(data)
+  console.log(this.request.file)
+  console.log(this.request.fils)
   res.end('hello from the server\n');
 }, {
   where: 'server'
